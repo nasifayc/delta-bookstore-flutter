7b4cb2d3d489ba113341d5038fdb20bd322b0bf7
nasifayc <nasifayc11@gmail.com>
2024-11-10 00:57:08 +0300

    data: static data created

---
+++ delta_ebookstore_app/lib/utils/static_data.dart
@@ -0,0 +1,70 @@
+class StaticData {
+  static List<Map<String, dynamic>> getbook() {
+    return [
+      {
+        "title": "Res Yelewim",
+        "author": "Elias Shitahun",
+        "year": 2000,
+        "coverImage": "assests/images/",
+        "hasAudio": true,
+        "rating": 4.5,
+        "genre": "Poetry",
+        "Purchases": 1000,
+        "language": "Amharic",
+        "reviews": [
+          {"username": "User 1", "rating": 5, "comment": "Great book!"},
+          {"username": "User 2", "rating": 4, "comment": "Average."},
+          {"username": "User 3", "rating": 3, "comment": "Needs improvement."}
+        ]
+      },
+      {
+        "title": "Yemisam Terara",
+        "author": "Fikremarkos Debebe",
+        "year": 1990,
+        "coverImage": "assests/images/",
+        "hasAudio": false,
+        "rating": 3.5,
+        "genre": "Poetry",
+        "Purchases": 1000,
+        "language": "Amharic",
+        "reviews": [
+          {"username": "User 1", "rating": 5, "comment": "Great book!"},
+          {"username": "User 2", "rating": 4, "comment": "Average."},
+          {"username": "User 3", "rating": 3, "comment": "Needs improvement."}
+        ]
+      },
+      {
+        "title": "Zubeyda",
+        "author": "Alex Abreham",
+        "year": 2006,
+        "coverImage": "assests/images/",
+        "hasAudio": true,
+        "rating": 4.8,
+        "genre": "Poetry",
+        "Purchases": 5456,
+        "language": "Amharic",
+        "reviews": [
+          {"username": "User 1", "rating": 5, "comment": "Great book!"},
+          {"username": "User 2", "rating": 4, "comment": "Average."},
+          {"username": "User 3", "rating": 3, "comment": "Needs improvement."}
+        ]
+      },
+      {
+        "title": "Book 1",
+        "author": "Author 1",
+        "year": 2000,
+        "coverImage": "assests/images/",
+        "hasAudio": true,
+        "rating": 4.5,
+        "genre": "Poetry",
+        "Purchases": 1000,
+        "language": "Amharic",
+        "reviews": [
+          {"username": "User 1", "rating": 5, "comment": "Great book!"},
+          {"username": "User 2", "rating": 4, "comment": "Average."},
+          {"username": "User 3", "rating": 3, "comment": "Needs improvement."}
+        ]
+      }
+    ];
+  }
+}
