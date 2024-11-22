11c833aa5958646bcbd403f16fbdf4be52441e32
nasifayc <nasifayc11@gmail.com>
2024-11-22 22:19:29 +0300

    Auth: authenticaton frontend and backend integrated

--- delta_ebookstore_app/lib/widgets/setting/user_info.dart
+++ delta_ebookstore_app/lib/widgets/setting/user_info.dart
@@ -1,5 +1,8 @@
+import 'package:delta_ebookstore_app/controllers/auth/auth_cubit.dart';
+import 'package:delta_ebookstore_app/controllers/auth/auth_state.dart';
 import 'package:delta_ebookstore_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserInfo extends StatelessWidget {
   const UserInfo({super.key});
@@ -7,32 +10,44 @@ class UserInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AppTheme theme = AppTheme.of(context);
-    return Column(
-      children: [
-        Center(
-          child: CircleAvatar(
-            radius: 80,
-            child: Image.asset("assets/images/boy.png"),
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        if (state is Authenticated) {
+          return Column(
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: 80,
+                  child: Image.asset("assets/images/boy.png"),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                state.user.name,
+                style: theme.typography.headlineMedium,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                state.user.phone ?? state.user.email!,
+                style: theme.typography.titleSmall,
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          );
+        }
+        return Center(
+          child: Text(
+            'Sign in Required!',
+            style: theme.typography.headlineSmall,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          'Nasifay',
-          style: theme.typography.headlineMedium,
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(
-          'nasifayc11@gmail.com',
-          style: theme.typography.titleSmall,
-        ),
-        const SizedBox(
-          height: 10,
-        )
-      ],
+        );
+      },
     );
   }
 }
